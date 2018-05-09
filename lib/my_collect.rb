bc4462600decc0e4f03706202b7c2fbb988f132e
James Thomas <jamesjacobthomas7@gmail.com>
2018-05-09 18:52:13 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,8 +4,8 @@ def my_collect(array)
   i = 0
   new_array = []
    while i < array.length
-  new_values = yield array[i]
-   new_array << new_values
+  new_array << yield(array[i])
+   
     i += 1
   end
   new_array
